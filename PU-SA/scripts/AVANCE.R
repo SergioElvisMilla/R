de90284e2a8156a434b0554ac8d91590fbba4ca1
SergioElvisMilla <sergio.elvis.milla@outlook.com>
2018-06-05 15:01:38 -0500

    commit05/06/2018-3

--- PU-SA/scripts/AVANCE.R
+++ PU-SA/scripts/AVANCE.R
@@ -1,8 +1,24 @@
 AUTO = read.csv("PU-SA/data/auto-mpg.csv", header = TRUE, sep = "," )   
 names(AUTO)  #PARA REVISAR LAS CABECERAS
 
+#para cargar archivos con separadores ";"
 read.csv2== read.csv("filename", sep =";", dec =",") # tambien puede ser sep="\t"    -----> para cargar archivos con separadores ";"
 
 
 
+# cuando no hay cabeceras
 
+auto_no_header<- read.csv("PU-SA/data/auto-mpg-noheader.csv", header = FALSE)  
+head(auto_no_header,4)
+
+# este ejemplo no se debe de hacer
+auto_no_sense<- read.csv("PU-SA/data/auto-mpg-noheader.csv")
+
+#
+auto_custom_header <- read.csv("PU-SA/data/auto-mpg-noheader.csv",
+                               header = FALSE, 
+                               col.names = c("numero","millas_por_galon","cilindrada",
+                                             "desplazamineto","caballos_de_potencia",
+                                             "peso","aceleracion","año","modelo",
+                                             )
+                               )
