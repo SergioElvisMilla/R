24b466994fc32f2c47cf859449378dcceba20852
SergioElvisMilla <sergio.elvis.milla@outlook.com>
2018-06-05 14:31:29 -0500

    commit05/06/18

--- PU-SA/scripts/AVANCE.R
+++ PU-SA/scripts/AVANCE.R
@@ -1,2 +1,8 @@
-AUTO = read.csv("..")
-#descubriendo r
+AUTO = read.csv("PU-SA/data/auto-mpg.csv", header = TRUE, sep = "," )   
+names(AUTO)  #PARA REVISAR LAS CABECERAS
+
+read.csv2== read.csv("filename", sep =";", dec =",") # tambien puede ser sep="\t"    -----> para cargar archivos con separadores ";"
+
+
+
+
