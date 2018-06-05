974741eccaa1cef3c06b882da85a1b432847e462
SergioElvisMilla <sergio.elvis.milla@outlook.com>
2018-06-05 15:02:49 -0500

    commit05/06/2018-04

--- PU-SA/scripts/AVANCE.R
+++ PU-SA/scripts/AVANCE.R
@@ -1,3 +1,4 @@
+#cargarndo un archivo a R
 AUTO = read.csv("PU-SA/data/auto-mpg.csv", header = TRUE, sep = "," )   
 names(AUTO)  #PARA REVISAR LAS CABECERAS
 
@@ -14,11 +15,23 @@ head(auto_no_header,4)
 # este ejemplo no se debe de hacer
 auto_no_sense<- read.csv("PU-SA/data/auto-mpg-noheader.csv")
 
-#
+# colocar columnas (customizar)
 auto_custom_header <- read.csv("PU-SA/data/auto-mpg-noheader.csv",
                                header = FALSE, 
                                col.names = c("numero","millas_por_galon","cilindrada",
                                              "desplazamineto","caballos_de_potencia",
-                                             "peso","aceleracion","año","modelo",
+                                             "peso","aceleracion","año","modelo"
                                              )
                                )
+head(auto_custom_header,4)
+
+
+# trabajar con valores especiales
+
+#NA: Not Available
+#na.strings=""
+#as.character()
+AUTO = read.csv("PU-SA/data/auto-mpg.csv", header = TRUE, sep = "," , strings.na = "", stringsAsFactors = FALSE )  
+
+
+
